3494d11fa8a0b368c8518e07b0015853e4dec681
Prakashom18 <shahomprakash097@gmail.com>
2026-01-12 22:35:59 +0545

    creating server

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const http = require('http');
-const cookieParser = require('cookie-parser');
+const {Chess} = require('chess.js')
+
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
 
 app.listen(3000,(err)=>{
     console.log("Running on port 3000");
