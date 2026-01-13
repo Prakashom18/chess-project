f0fdd86af547d711c9d3c113824a5c0731033f6b
Prakashom18 <shahomprakash097@gmail.com>
2026-01-13 19:26:29 +0545

    index page

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const http = require('http');
+const socket = require('socket.io');
 const {Chess} = require('chess.js')
+const path = require('path');
 
 const app = express();
+
 const server = http.createServer(app);
 const io = socket(server);
 
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
 
-app.listen(3000,(err)=>{
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/',(req,res)=>{
+    res.render('index',{title:"Chess Game"});
+})
+
+
+
+server.listen(3000,(err)=>{
     console.log("Running on port 3000");
 })
