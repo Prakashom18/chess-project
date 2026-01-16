4720ab3ba35aa66eae800df44256aed3ddd03ec4
Prakashom18 <shahomprakash097@gmail.com>
2026-01-16 18:13:18 +0545

    chessgame

--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -6,6 +6,35 @@ let draggedPiece = null;
 let sourceSquare = null;
 let playerRole = null;
 
+const renderBoard = ()=>{
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row,rowindex)=>{
+        row.forEach((square,squareindex)=>{
+            // console.log(square);
+            const squareElement = document.createElement('div');
+            squareElement.classList.
+            add('square',(rowindex+squareindex)%2 === 0 ? "light" : "dark");
+            squareElement.dataset.row = rowindex;
+            square.dataset.col=squareindex;
+
+            if(square){
+                const pieceElement = document.createElement('div');
+            }
+        })
+    })
+};
+
+const handleMove = () =>{
+
+}
+
+const getPieceUnicode = () =>{
+
+};
+
+renderBoard();
+
 // socket.emit("churan");
 // socket.on("churan papdi",function(){
 //     console.log("churan papdi recieved"); 
